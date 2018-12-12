d0ff733ca3ab0f0e5aac0c6d88f9883541c4e810
Gabriel Ng <gabrielng15@gmail.com>
2018-12-12 14:30:59 -0800

    Set up client react directory (#12)

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="main">
+        <h1>Hello Friend</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
