b989870c2f407c8e707524d77d1d7ed8fb7ec67c
Gabriel Ng <gabrielng15@gmail.com>
2018-12-11 22:04:32 -0800

    Created rootquery for graphql (#4)

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -3,6 +3,7 @@ const graphql = require('graphql');
 const { 
     GraphQLObjectType,
     GraphQLString,
+    GraphQLSchema,
 } = graphql;
 
 const BookType = new GraphQLObjectType({
@@ -13,3 +14,20 @@ const BookType = new GraphQLObjectType({
         genre: { type: GraphQLString }
     })
 });
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: { id: { type: GraphQLString } },
+            resolve(parent, args) {
+                // code to get data from db / other source
+            }
+        }
+    }
+});
+
+moduke.exports = new GraphQLSchema({
+    query: RootQuery,
+})
