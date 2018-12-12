372ee0d26c048551658ff69e397b65234a7e6140
Gabriel Ng <gabrielng15@gmail.com>
2018-12-11 21:53:10 -0800

    Created BookType in schema (#3)

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,1 +1,15 @@
 const graphql = require('graphql');
+
+const { 
+    GraphQLObjectType,
+    GraphQLString,
+} = graphql;
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields: () => ({
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString }
+    })
+});
